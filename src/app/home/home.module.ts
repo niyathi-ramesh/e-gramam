3df8f0d83fe1681f91022603f94b70d33ed3d074
niyathi-ramesh <niyathirameshhere@gmail.com>
2021-09-01 21:18:11 +0530

    Implement carousel in home page.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IvyCarouselModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
